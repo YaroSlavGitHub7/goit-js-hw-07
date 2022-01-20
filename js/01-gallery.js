20efaf81dbc3346da1f6d34ea972622cd8021c51
YaroSlavGitHub7 <yatret74@gmail.com>
2022-01-21 00:12:03 +0200

    task7_1
    создание и рендер разметки

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+//1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// Реализация делегирования на div.gallery и получение url большого изображения.
+// Подключение скрипта и стилей библиотеки модального окна basicLightbox.Используй CDN сервис jsdelivr 
+// и добавь в проект ссылки на минифицированные(.min) файлы библиотеки.
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// Замена значения атрибута src элемента < img > в модальном окне перед открытием. 
+// Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+
+// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе <img>, 
+// и указываться в href ссылки. 
+
+// Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию 
+// пользователь будет перенаправлен на другую страницу.Запрети это поведение по умолчанию.
+
+// видео Репеты https://www.youtube.com/watch?v=pNxBoxbnTVA&t=3623s (создание КалорсПикер)
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// рендер разметки
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// создание разметки
+console.log(createGalleryItemsMarkup(galleryItems));
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return /*шаблон одного элемента*/ ` 
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>
+        `;
+    })
+        .join('');
+    // console.log(galleryItems);
+}
+
