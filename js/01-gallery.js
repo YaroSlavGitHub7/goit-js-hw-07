bbb95c7c9510d0f2aac28984c68de70419194a48
YaroSlavGitHub7 <yatret74@gmail.com>
2022-01-23 18:21:59 +0200

    task7.2
    the end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,13 @@
 //1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.+
-// Реализация делегирования на div.gallery и получение url большого изображения.
+// Реализация делегирования на div.gallery и получение url большого изображения.+
 // Подключение скрипта и стилей библиотеки модального окна basicLightbox.Используй CDN сервис jsdelivr 
-// и добавь в проект ссылки на минифицированные(.min) файлы библиотеки.
-// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// и добавь в проект ссылки на минифицированные(.min) файлы библиотеки.+
+// Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.+
 // Замена значения атрибута src элемента < img > в модальном окне перед открытием. 
-// Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+// Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.+
 
 // Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе <img>, 
-// и указываться в href ссылки. 
+// и указываться в href ссылки. +
 
 // Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию +
 // пользователь будет перенаправлен на другую страницу.Запрети это поведение по умолчанию.
@@ -15,6 +15,7 @@
 // видео Репеты https://www.youtube.com/watch?v=pNxBoxbnTVA&t=3623s (создание КалорсПикер)
 
 import { galleryItems } from './gallery-items.js';
+    // console.log(galleryItems);
 // Change code below this line
 
 // рендер разметки
@@ -42,7 +43,6 @@ function createGalleryItemsMarkup(galleryItems) {
         `;
     })
         .join('');
-    // console.log(galleryItems);
 }
 
 // делегированиe и получение url большого изображения
@@ -52,13 +52,13 @@ galleryContainer.addEventListener('click', onGalleryContainerClick, );
 function onGalleryContainerClick(evt) {
     evt.preventDefault(); /*запрет перехода по ссылке*/
     const onGalleryIimageClick = evt.target.classList.contains('gallery__image');
-        if (!onGalleryIimageClick) { /*кликаем только на ребенка с классом 'gallery__image'*/
+        if (!onGalleryIimageClick) { /*кликаем только на потомка с классом 'gallery__image'*/
             return;
         };   
-        // console.log(evt.target.dataset.source);
+        console.log(evt.target.dataset.source);
     // Использ  модального окна библиотеки basicLightbox
     const instance = basicLightbox.create(`
         <img src="${evt.target.dataset.source}" width="800" height="600">
-    `)
+    `);
     instance.show();
 }
